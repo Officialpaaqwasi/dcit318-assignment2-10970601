52abf10fa8f579a5bd90818a7fcb07a0a0d603f7
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-06-17 17:10:45 +0000

    Inheritance complete

---
+++ Inheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myAnimal.MakeSound();
+            myDog.MakeSound();
+            myCat.MakeSound();
+        }
+    }
+}
