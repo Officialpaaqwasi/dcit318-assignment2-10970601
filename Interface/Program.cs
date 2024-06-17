505f1a9a7245e6b93074961da7ccb347642f8c53
officialpaaqwasi <iklutse@st.ug.edu.gh>
2024-06-17 17:19:24 +0000

    Interface complete

---
+++ Interface/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+ interface IMovable
+ {
+  void Move();
+ }
+
+ class Car : IMovable
+ {
+  public void Move()
+  {
+   Console.WriteLine("Car is moving");
+  }
+ }
+
+ class Bicycle : IMovable
+ {
+  public void Move()
+  {
+   Console.WriteLine("Bicycle is moving");
+  }
+ }
+
+ class Program
+ {
+  static void Main(string[] args)
+  {
+   IMovable myCar = new Car();
+   IMovable myBicycle = new Bicycle();
+
+   myCar.Move();
+   myBicycle.Move();
+  }
+ }
+}
